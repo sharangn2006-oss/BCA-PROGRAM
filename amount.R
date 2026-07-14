5b7c7a9cd6c423f8b2d82ef6bed2394f7ed24b43
Sharandeep G N <sharandeepgn2002@gmail.com>
2026-07-14 12:05:32 +0530

    Assesment Question 1-4

---
+++ amount.R
@@ -0,0 +1,11 @@
+principal <-10000
+rate <- 8.5
+time <-3
+
+simple_interest<- (principal *rate*time)/100
+total_amount <-  principal +simple_interest
+
+cat("principal :Rs.",principal,"\n")
+cat("Simple Interest :Rs.",simple_interest,"\n")
+cat("Total Amount:Rs.",total_amount,"\n")
+
---
+++ assessment.R
@@ -0,0 +1,34 @@
+#QUE 1
+student_name <- c("pooja", "pavan","sharan")
+age<-c(56,79,100)
+is_enrolled <- c(TRUE,FALSE,TRUE)
+
+#QUE 2
+x=15
+y=7
+z=3
+
+result=(x+y)*z-(x/y)
+print(result)
+
+#QUE 3
+a<-10
+b<-a
+a<-20
+
+print(b)
+
+#QUE 4
+a<-10
+2->b
+x=10
+y=12
+rm(x)
+result=(x=0+a+b)
+#result2=(y+x)
+print(result)
+#print(result2)
+
+
+ 
+
